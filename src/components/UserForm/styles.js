56ed56fecf9c646b44893c5b2bc79ebeb74f84c4
eduardo lasso <elassol@hotmail.com>
2021-02-07 18:53:41 +0100

    add avatar image and field to edit user

--- src/components/UserForm/styles.js
+++ src/components/UserForm/styles.js
@@ -12,8 +12,8 @@ export const EditPageWrapper = styled.div`
   display: block;
   max-width: 100%;
   margin: 0 auto;
-  padding: 25px 15px;
-  border: 2px solid #034f84;
+  padding: 0;
+
 `;
 
 export const SubTitlePage = styled.h2`
@@ -21,6 +21,7 @@ export const SubTitlePage = styled.h2`
   font-size: 18px;
   line-height: 21px;
   letter-spacing: 0.5px;
+  margin: 0 0 12px;
 `;
 
 export const FormBlock = styled.div`
@@ -38,6 +39,26 @@ export const InputFields = styled.input`
   }
 `;
 
+export const ButtonStyle = styled.button`
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 16px;
+  border: none;
+  outline: 0;
+  display: block;
+  padding: 8px 11px;
+  color: white;
+  background-color: #034f84;
+  text-align: center;
+  cursor: pointer;
+  font-size: 15px;
+  text-decoration: none;
+  margin-bottom: 12px;
+
+  &:hover {
+    background-color: #92a8d1;
+  }
+`;
+
 export const LabelFieldsForm = styled.span`
   display: block;
   margin: 0 0 5px;
@@ -46,4 +67,29 @@ export const LabelFieldsForm = styled.span`
   line-height: 16px;
   letter-spacing: 0.5px;
   color: #034f84;
+`;
+
+export const FormStyles = styled.form`
+  display: block;
+  width: 35%;
+  padding: 0 24px;
+`;
+
+export const FormWarpper = styled.div`
+  display: flex;
+  border: 2px solid #034f84;
+  background: #f7cac9;
+`;
+
+export const AvatarWrapper = styled.div`
+  display: block;
+  width: 65%;
+  height: auto;
+  margin: 0;
+
+  & > img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
 `;
